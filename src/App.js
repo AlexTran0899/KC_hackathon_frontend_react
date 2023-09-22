715d6caa548180f41dd51c527c609e36fd9cafb6
Alextran0899 <alextran0899@gmail.com>
2023-09-22 11:32:03 -0500

    added chacrui form

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 import { useEffect, useState } from "react";
 import axios from "axios";
 import UserCard from './UserCard'
-import { SimpleGrid } from '@chakra-ui/react';
+import { SimpleGrid,  FormControl,
+  FormLabel,
+  Field,
+Input,
+Button} from '@chakra-ui/react';
 
 const defaultFormData = {
   username: "",
@@ -21,44 +25,59 @@ function App() {
   const onSubmit = e => {
     e.preventDefault()
     axios.post("http://localhost:9000/api/users", formData)
-        .then(res => {
-          users.push(formData)
-          setFormData(defaultFormData)
-        })
-        .catch(e => console.log(e))
+      .then(res => {
+        users.push(formData)
+        setFormData(defaultFormData)
+      })
+      .catch(e => console.log(e))
   }
+
   const onChange = e => {
-    const {name,value} = e.target
-    setFormData(prev => ({...prev,[name]: value}))
+    const { name, value } = e.target
+    setFormData(prev => ({ ...prev, [name]: value }))
   }
   return (
-    <div className="App" style={{margin:"50px"}}>
+    <div className="App" style={{ margin: "50px" }}>
       <header className="App-header">
         <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
           {users && users.map(eachUser => <UserCard userData={eachUser} />)}
         </SimpleGrid>
-        <form action="" onSubmit={onSubmit}>
-          <label htmlFor="username">Username</label>
-          <input style={{border: "1px solid black"}}
-                 type="text"
-                 name="username"
-                 value={formData.username}
-                 onChange={onChange}
-          />
-          <br/>
-          <label htmlFor="password">passwords</label>
-          <input style={{border: "1px solid black"}}
-                 type="password"
-                 name="password"
-                 value={formData.password}
-                 onChange={onChange}
-          />
-          <br/>
-          <button
-              style={{border: "1px solid black"}}
-              type="submit">submit
-          </button>
-        </form>
+        <form onSubmit={onSubmit}>
+    
+        <FormControl >
+            <FormControl >
+              <FormLabel>username</FormLabel>
+              <Input
+                focusBorderColor='pink.400'
+                placeholder='Here is a sample placeholder'
+                type="text"
+                name="username"
+                value={formData.username}
+                onChange={onChange}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>password</FormLabel>
+              <Input
+                focusBorderColor='pink.400'
+                placeholder='Here is a sample placeholder'
+                type="password"
+                value={formData.password}
+                name="password"
+                onChange={onChange}
+              />
+            </FormControl>
+          
+        </FormControl>
+        <Button
+            mt={4}
+            colorScheme='teal'
+            type='submit'
+          >
+            Submit
+          </Button>
+          </form>
+
       </header>
     </div>
   );
